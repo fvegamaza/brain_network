6d1b29df69fe01389ab5729a7fae4a9874b66520
Fvega <fvegamaza@gmail.com>
2021-05-08 21:45:18 -0300

    First activity done

--- first_attempt.py
+++ first_attempt.py
@@ -1,3 +1,11 @@
 import pandas as pd
 path = "C:/Users/fvegamaza/PycharmProjects/challenger_brain_network"
+
 df = pd.read_spss(path + "/Base Ejercicio BRN 2020.sav")
+df["responseID"] = df["responseID"].astype(int)
+
+df = df.melt(id_vars=['responseID'])
+df = df.sort_values(by = ["responseID","variable"])
+df = df.rename(columns={'value':'valor'})
+
+df.to_csv("BRN 2020.csv",sep=",", header=True, )
