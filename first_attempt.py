971bd4b000d508e4f7c9c7c086856a0a27578b20
Fvega <fvegamaza@gmail.com>
2021-05-10 17:42:13 -0300

    First activity done

--- first_attempt.py
+++ first_attempt.py
@@ -7,5 +7,9 @@ df["responseID"] = df["responseID"].astype(int)
 df = df.melt(id_vars=['responseID'])
 df = df.sort_values(by = ["responseID","variable"])
 df = df.rename(columns={'value':'valor'})
+df = df.set_index("responseID")
+df.to_csv("BRN 2020.csv",sep=",", header=True)
 
-df.to_csv("BRN 2020.csv",sep=",", header=True, )
+
+
+df[df["variable"] == "Q24"].value_counts() #Para graficar antiguedad
