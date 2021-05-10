2e3469082dc10656e26b929327769e787728fec2
Fvega <fvegamaza@gmail.com>
2021-05-10 20:11:09 -0300

    Q48 feature cleaned

--- first_attempt.py
+++ first_attempt.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import re
 path = "C:/Users/fvegamaza/PycharmProjects/challenger_brain_network"
 
 df = pd.read_spss(path + "/Base Ejercicio BRN 2020.sav")
@@ -12,4 +13,14 @@ df.to_csv("BRN 2020.csv",sep=",", header=True)
 
 
 
-df[df["variable"] == "Q24"].value_counts() #Para graficar antiguedad
+df1 = df[df["variable"] == "Q24"] #Para obtener la antiguedad con sus respectivos ids
+df1.to_csv("antiguedad.csv",sep=",", header=True)
+
+
+
+df2 = df[df["variable"].str.contains("Q48")].copy(deep= True) #Filter by string https://stackoverflow.com/questions/48020296/how-to-filter-pandas-dataframe-by-string
+df2.isna().sum() #4454 na - Asumo que cuando las respuesta multiples no se marca se lo computa como na
+df2 = df2.dropna()
+df2["valor"].value_counts() #402 string vacios - verificaria si no hay un error en el procesamiento
+df2["valor"].iloc[3] #ejemplo
+df2 = df2[df2["valor"] != '']
