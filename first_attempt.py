d734fd91daae4280ae81b773d1e5719240bb5324
Fvega <fvegamaza@gmail.com>
2021-05-11 07:30:59 -0300

    Comments done

--- first_attempt.py
+++ first_attempt.py
@@ -1,33 +1,41 @@
-import pandas as pd
-import re
-path = "C:/Users/fvegamaza/PycharmProjects/challenger_brain_network"
+# Hola! Mi intención en este script es comentar como fui haciendo las tablas para luego obtener los graficos en tableau.
 
+
+
+
+import pandas as pd
+#Lectura de datos
+path = "C:/Users/fvegamaza/PycharmProjects/challenger_brain_network"
 df = pd.read_spss(path + "/Base Ejercicio BRN 2020.sav")
+#Transformo a enteros el ID
 df["responseID"] = df["responseID"].astype(int)
 
+#Actividad 1
 df = df.melt(id_vars=['responseID'])
+#Es usual para mi usar el Melt(), por lo que lo conocía.
+#Sobretodo en algunas predicciones porque es necesario para tener eventos unicos formando el id del usuario(acá el melt) y luego un merge con alguna marca temporal
+
 df = df.sort_values(by = ["responseID","variable"])
 df = df.rename(columns={'value':'valor'})
 df = df.set_index("responseID")
-df.to_csv("BRN 2020.csv",sep=",", header=True)
+df.to_csv("BRN 2020.csv",sep=",", header=True) #Aquí la actividad 1 es copletada
 
 
-
+#Q24 Antiguedad
 df1 = df[df["variable"] == "Q24"] #Para obtener la antiguedad con sus respectivos ids
 df1.to_csv("antiguedad.csv",sep=",", header=True)
 
-
-
-df2 = df[df["variable"].str.contains("Q48")].copy(deep= True) #Filter by string https://stackoverflow.com/questions/48020296/how-to-filter-pandas-dataframe-by-string
-df2.isna().sum() #4454 na - Asumo que cuando las respuesta multiples no se marcan se lo computa como nan
+#Q48 Banco Operativo
+df2 = df[df["variable"].str.contains("Q48")].copy(deep= True) #No recordaba como hacerlo, lo obtuve de aquí: Filter by string https://stackoverflow.com/questions/48020296/how-to-filter-pandas-dataframe-by-string
+df2.isna().sum() #4454 na - Asumo que cuando las respuestas multiples no se marcan se lo computa como nan
 df2 = df2.dropna()
-df2["valor"].value_counts() #402 string vacios - verificaria si no hay un error en el procesamiento
+df2["valor"].value_counts() #402 string vacios
 df2["valor"].iloc[3] #ejemplo
-df2 = df2[df2["valor"] != '']
+df2 = df2[df2["valor"] != ''] #Me quedo con todos los string diferentes a ''
 df2["valor"].value_counts() #Esta ok para exportar
-df2.to_csv("Q48.csv",sep=",", header=True, encoding= "utf-8")
+df2.to_csv("Q48.csv",sep=",", header=True, encoding= "utf-8") # Con esta tabla elaboré los graficos acerca en qué banco operan los encuestados
 
-
+#Q49 Banco Principal
 df3 = df[df["variable"] == "Q49"].copy(deep=True)
 df3.value_counts()
 df3.valor.iloc[6] #Limpio errores de codificacion
@@ -35,15 +43,17 @@ df3 = df3["valor"].replace('17. ITAÃš', "17. ITAU").replace('9. NACIÃ“N','9
 df3 = pd.DataFrame(df3)
 df3.to_csv("Q49.csv", sep= ",", header=True)
 
+
+#Q52 Recomendacion
 df4 = df[df["variable"].str.contains("Q52")].copy(deep= True)
 df4.isna().sum() #4454 na - Asumo que cuando las respuesta multiples no se marcan se lo computa como nan
 df4 = df4.dropna()
 df4["variable"].value_counts()
 
-#En este momento al comparar el df4 y el df2 correspontientes a las preguntas A13 Y A9 respectivamente
-# me di cuenta que la segunda parte del nombre de las variables "Q52R7" que el velor luego de la R corresponde
+#En este momento al comparar el df4 y el df2 correspondientes a las preguntas A13 Y A9 respectivamente
+# me di cuenta que la segunda parte del nombre de las variables "Q52R7", el valor luego de la R corresponde
 # al numero de celda y no necesariamente a la codificacion por banco presentada en la columnas
-# Por ejemplo el banco Patagonia tiene asignado el numero 13, pero en el df las filas correspondientes a este banco son las "R10"
+# Por ejemplo, el banco Patagonia tiene asignado el numero 13 en el cuestionario, pero en el df las filas correspondientes a este banco son las "R10"
 
 listado= {
 "Q52R1":"SANTANDER",
@@ -62,31 +72,35 @@ listado= {
 "Q52R14":"SUPERVIELLE",
 "Q52R15":"MUNICIPAL DE ROSARIO",
 "Q52R17":"BANCOR",
-"Q52R30":"nan"} #Esto lo hice con un editor de texto
-df4= df4.replace({"variable":listado})
+"Q52R30":"nan"} #Esto lo hice con un editor de texto, es solo para identificar los bancos
+df4= df4.replace({"variable":listado}) #Aqui reemplazo
 df4["variable"].value_counts()
 
-df4["valor2"] = "reemplazar"
-df4.drop((df4[df4["valor"] == 'NS/NC'].index),inplace=True)
+df4.drop((df4[df4["valor"] == 'NS/NC'].index),inplace=True) #Asumo como Nulos las resppuestas "NS/NC"
 
-df4["valor"] = df4["valor"].astype(int)
+df4["valor2"] = "reemplazar" # Creo una nueva columna para reemplazar luego
+
+df4["valor"] = df4["valor"].astype(int) #Me fue necesario convertirlo a int para utilizar operadores logicos
+
+#La siguiente parte del codigo fue la solución que encontré para modificar "manualmente" los bins, dado que
+#luego de intentar en Tableau no logré hacerlo como espeficicaba la consigna. Este codigo hace el trabajo igualmente:
 
 df4.loc[df4["valor"] <= 6, "valor2"] = "1 a 6"
 df4.loc[df4["valor"] == 7, "valor2"] = "7 y 8"
 df4.loc[df4["valor"] == 8, "valor2"] = "7 y 8"
 df4.loc[df4["valor"] == 9, "valor2"] = "9 y 10"
 df4.loc[df4["valor"] == 10, "valor2"] = "9 y 10"
-
 df4.to_csv("Q52.csv", sep= ",", header=True)
 
 #Q51 #Satisfacción
-
-df5 = df[df["variable"].str.contains("Q51")].copy(deep= True)
+df5 = df[df["variable"].str.contains("Q51")].copy(deep= True) #El mismo filtrado que las variables anterior. Si contiene "Q51" sin importar lo siguiente es util en este punto
 df5.isna().sum() #4454 na
 df5 = df5.dropna()
 df5.drop((df5[df5["valor"] == 'NS/NC'].index),inplace=True)
-df5["valor"].value_counts() #402 string vacios - verificaria si no hay un error en el procesamiento
+df5["valor"].value_counts() #402 string vacios
 
+# Dado que ya utilicé un listado para matchear y que las respuestas en el cuestionarios de a12 y a13 se responden en concordancia a lo respondido en a9
+# Hice un outer join basandome en el ID de los encuestados para luegos sacar el promedio
 df5_df4_outer = pd.merge(df4, df5, on='responseID', how='outer')
 del df5_df4_outer["valor2"],df5_df4_outer["variable_y"]
 df5_df4_outer = df5_df4_outer.dropna()
